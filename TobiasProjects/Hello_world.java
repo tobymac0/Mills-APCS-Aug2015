2e3c7b30f95ae93800d9aea505965481a14b7e68
tobymac0 <tmjeung@yahoo.com>
2015-08-17 09:21:09 -0700

    initial commit

---
+++ TobiasProjects/Hello_world.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Hello_world here.
+ * 
+ * @author (Tobias) 
+ * @version (1.0 08/17/2015
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Tobias"); //Display the string
+    }
+}
