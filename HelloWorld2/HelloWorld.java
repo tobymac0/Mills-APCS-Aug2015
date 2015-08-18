b268c7eee43dbfa324d446eae506e40e5e94c757
tobymac0 <tmjeung@yahoo.com>
2015-08-18 09:18:50 -0700

    Hello World demonstration August 8th, 2015
    Programming is cool.

---
+++ HelloWorld2/HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * This will do a simple println. Our first java code. Just do it.
+ * 
+ * @author Tobias Jeung
+ * @version 08/18/2015
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Tobias Jeung deez nuts"); 
+
+    }
+
+}
