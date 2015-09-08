e10c14b710585cd42fcc493c3ea153bf2dd2ff09
tobymac0 <tmjeung@yahoo.com>
2015-09-08 09:21:37 -0700

    f
    f

---
+++ Fahrenheit/celsius.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class celsius here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class celsius
+{
+   public static void main(String[] args)  {
+       // declare variables
+       double fahrenheit;
+       double celsius;
+       fahrenheit= celsius*1.8+32;
+       System.out.println (fahrenheit);
+       
+       
+       
+    }
+}
