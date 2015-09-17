a0f882d4135a21cc13f88582ab510c7022d7008e
tobymac0 <tmjeung@yahoo.com>
2015-09-17 08:48:51 -0700

    yes

---
+++ TobiasProjects/oldlady.java
@@ -0,0 +1,68 @@
+
+/**
+ * Write a description of class oldlady here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class oldlady
+{
+    public static void methodfly() {
+    System.out.println("There was an old lady who swallowed a fly");
+}
+    public static void methodflylady()  {
+        System.out.println("I don't know why she swallowed the fly");
+        System.out.println("Perhaps she'll die");
+    }
+    public static void methodspider()  {
+        System.out.println("There was an old lady who swallowed a spider,");
+    }
+    public static void methodspiderlady()  {
+        System.out.println("that wiggled and wiggled and tickled inside her.");
+        System.out.println("she swallowed the spider to catch the fly");
+    }
+    public static void methodbird()  {
+        System.out.println("There was an old lady who swallowed a bird");
+    }
+    public static void methodbirdlady()  {
+        System.out.println("How absurd to swallow a bird.");
+        System.out.println("She swallowed the bird to catch the spider");
+    }
+    public static void methodcat()  {
+        System.out.println("There was an old lady who swallowed a cat");
+    }
+    public static void methodcatlady()  {
+        System.out.println("Imagine that, she swallowed a cat.");
+        System.out.println("She swallowed the cat to catch the bird");
+    }
+    public static void methoddog()  {
+        System.out.println("There was an old lady who swallowed a dog");
+    }
+    public static void methoddoglady()  {
+        System.out.println("My what a hog, to swallow a dog.");
+        System.out.println("She swallowed the dog to catch the cat");
+    }
+    public static void main(String[] args)  {
+        methodfly();
+        methodflylady();
+        methodspider();
+        methodspiderlady();
+        methodflylady();
+        methodbird();
+        methodbirdlady();
+        methodspiderlady();
+        methodflylady();
+        methodcat();
+        methodcatlady();
+        methodbirdlady();
+        methodspiderlady();
+        methodflylady();
+        methoddog();
+        methoddoglady();
+        methodcatlady();
+        methodbirdlady();
+        methodspiderlady();
+        methodflylady();
+        
+    }
+}
