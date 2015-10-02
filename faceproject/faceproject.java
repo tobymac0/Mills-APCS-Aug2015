6505bd340472c878b9daf92fb783c35edfcfb2c8
tobymac0 <tmjeung@yahoo.com>
2015-10-02 08:01:33 -0700

    face

--- faceproject/faceproject.java
+++ faceproject/faceproject.java
@@ -9,7 +9,7 @@ public class faceproject
 {
     //make top of head and bottom
    public static void topofhead()  {
-       for(int i=1; i<=20; i++)  {
+       for(int i=1; i<=40; i++)  {
            System.out.print("-");
         }
         System.out.println(" ");
@@ -17,7 +17,7 @@ public class faceproject
     //print sides of faces
     public static void doublesideface()  {
         System.out.print("|");
-        for(int i=1; i<=20; i++)  {
+        for(int i=1; i<=40; i++)  {
             System.out.print(" ");
         }
         System.out.print("|");
@@ -25,22 +25,103 @@ public class faceproject
     }
     //make eyes
     public static void eyes()  {
-        for(int i=1; i<=3; i++)  {
+        for(int i=1; i<=6; i++)  {
             System.out.print(" ");
         }
         System.out.print("/-\\");
-        for(int j=1; j<=8; j++)  {
+        for(int j=1; j<=16; j++)  {
             System.out.print(" ");
         }
         System.out.print("/-\\");
         System.out.println(" ");
     }
-    public static void 
+    public static void nose()  {
+        {
+        for(int i=1; i<=16; i++)  {
+            System.out.print(" ");
+        }
+        System.out.print("|-|");
+    }
+    for(int j=1; j<=10; j++)  {
+        System.out.print(" ");
+    }
+    System.out.println();
+}
+public static void writeChars(char ch, int number)  {
+    for(int i=1; i<=number; i++)  {
+        System.out.print(ch);
+    }
+}
+public static void mouth()  {
+    for(int i=1; i<=5; i++)  {
+        writeChars('<', i);
+        writeChars(' ', 10-2*i);
+        writeChars('>', i);
+        System.out.println();
+    }
+}
+//maketeeth
+public static void teeth()  {
+    for(int i=1; i<=8; i++)  {
+        System.out.print(" ");
+    }
+    System.out.print("|_|_|");
+    System.out.println();
+}
+//makelegs
+public static void legs()  {
+    for(int i=1; i<=8; i++) {
+        System.out.print(" ");
+    }
+    System.out.print("|    |");
+    for(int j=1; j<=4; j++)  {
+        System.out.print(" ");
+    }
+    System.out.print("|    |");
+    System.out.println();
+}
+
+//feet
+public static void feet()  {
+    {
+    for(int i=1; i<=8; i++)  {
+        System.out.print(" ");
+    }
+    System.out.print("|===|");
+}
+for(int i=1; i<=6; i++)  {
+    System.out.print(" ");
+}
+System.out.print("|===|");
+System.out.println();
+}
+
+
+  
+    
     public static void main(String[] args)  {
         topofhead();
         doublesideface();
         doublesideface();
         eyes();
+        nose();
+        doublesideface();
+        doublesideface();
+        doublesideface();
+        doublesideface();
+        doublesideface();
+        mouth();
+        teeth();
+        doublesideface();
+        topofhead();
+        doublesideface();
+        doublesideface();
+        topofhead();
+        legs();
+        legs();
+        legs();
+        feet();
+        
     }
 }
 
