2f96d70cdf287913c1afe43699d18e554606b67b
tobymac0 <tmjeung@yahoo.com>
2015-10-15 08:06:42 -0700

    faces are

--- faceproject/exerciseonepony.java
+++ faceproject/exerciseonepony.java
@@ -1,33 +1,35 @@
 
 /**
- * Write a description of class exerciseonepony here.
+ * my little pony project 
+ * Tobias Jeung  
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class exerciseonepony
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class exerciseonepony
-     */
-    public exerciseonepony()
-    {
-        // initialise instance variables
-        x = 0;
+    //exerciseone generate distances from each pair of coordinates 
+   public static void getdistances()  {
+       System.out.println("Distance from Baltimore + Manehattan is " + distance(31,-16,34,-19); 
+       System.out.println("Distance from Los Pegasus to Neighagra Falls is " + distance(7,-19,22,-8); 
+       System.out.pritnln("Distance from the Badlands to Ponyville is " + distance(25,-23,16,-14);
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public double distance(int x1, int x2, int y1, int y2)  {
+        double distance = Math.sqrt(Math.pow((x2-x1),2 + Math.pow((y2-y1,2));
+        return distance;
     }
-}
+    
+    
+    
+    //exercisetwo finding circumfrence
+    public double roadTrip(double diameter)  {
+        double length diameter*Math.PI; 
+        return length;
+    }
+    
+    //exercisethree output distance between the three destinations and angles between each pair of coordinates
+    public static void threedistance()  {
+    }
+    
+    //exercisefour returns longest distance between any pair of destinations 
+    // execute public double distance return distance from los pegasus to Neighagra galls
+   
+    
